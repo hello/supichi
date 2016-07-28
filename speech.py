172c7908f006ed2b4a38c8fad8e2bca58e229aa3
kingshy <kingshy@sayhello.com>
2016-07-27 17:03:17 -0700

    add demo config

--- speech.py
+++ speech.py
@@ -115,7 +115,7 @@ if __name__ == '__main__':
         pb = sys.argv[4]
 
     su = SlowUpload(filename)
-    headers = {"content-type": "application/octet-stream"} #, "X-Hello-Sense-Id": "8AF6441AF72321F4"}
+    headers = {"content-type": "application/octet-stream", "X-Hello-Sense-Id": "C8DAAC353AEFA4A9"}
     if env == 'local':
         ENDPOINT = "http://localhost:8181/upload/audio?r=%s&pb=%s" % (sampling_rate, pb)
     elif env == 'dev':
