968613e7f611faebd04aff373ddfbc1b152afa71
kingshy <kingshy@sayhello.com>
2016-08-01 19:32:50 -0700

    first draft of downsampling in java, no header created

--- supichi-core/src/main/java/is/hello/speech/core/text2speech/Text2SpeechQueueConsumer.java
+++ supichi-core/src/main/java/is/hello/speech/core/text2speech/Text2SpeechQueueConsumer.java
@@ -1,7 +1,9 @@
 package is.hello.speech.core.text2speech;
 
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.PutObjectResult;
 import com.amazonaws.services.sqs.AmazonSQSAsync;
 import com.amazonaws.services.sqs.model.DeleteMessageBatchRequest;
 import com.amazonaws.services.sqs.model.DeleteMessageBatchRequestEntry;
@@ -11,23 +13,26 @@ import com.amazonaws.services.sqs.model.ReceiveMessageRequest;
 import com.amazonaws.services.sqs.model.ReceiveMessageResult;
 import com.google.common.base.Optional;
 import com.google.common.collect.Lists;
-import com.google.common.io.ByteStreams;
 import com.ibm.watson.developer_cloud.text_to_speech.v1.TextToSpeech;
 import com.ibm.watson.developer_cloud.text_to_speech.v1.model.AudioFormat;
 import com.ibm.watson.developer_cloud.text_to_speech.v1.model.Voice;
+import com.ibm.watson.developer_cloud.text_to_speech.v1.util.WaveUtils;
 import io.dropwizard.lifecycle.Managed;
 import is.hello.speech.core.api.Text2SpeechQueue;
 import is.hello.speech.core.configuration.SQSConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
-import java.io.FileOutputStream;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.ws.rs.core.MediaType;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
+import java.util.UUID;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.TimeUnit;
 
 /**
  * Created by ksg on 6/28/16
@@ -38,6 +43,7 @@ public class Text2SpeechQueueConsumer implements Managed {
     private static final int MAX_RECEIVED_MESSAGES = 10;
     private static final AudioFormat DEFAULT_AUDIO_FORMAT = AudioFormat.WAV;
     private static final int WATSON_SAMPLING_RATE = 22050;
+    private static final float TARGET_SAMPLING_RATE = 16000.0f;
 
     private static final String COMPRESS_PARAMS = "-r 16k -e ima-adpcm `";
 
@@ -132,24 +138,63 @@ public class Text2SpeechQueueConsumer implements Managed {
                         LOGGER.debug("action=save-audio-to-file filename={}", keyname);
 
                         // Save Raw data to tmp file, upload tmp file to S3
-                        final String rawFilename = "/tmp/tmp.wav";
-                        final File rawFile = new File(rawFilename);
-                        final FileOutputStream fileOutputStream = new FileOutputStream(rawFile);
-                        ByteStreams.copy(stream, fileOutputStream);
-                        fileOutputStream.close();
-
-                        final String s3RawBucket = String.format("%s/%s/%s", s3KeyRaw, synthesizeMessage.getIntent().toString(), synthesizeMessage.getCategory().toString());
-                        amazonS3.putObject(new PutObjectRequest(s3RawBucket, String.format("%s-raw.wav", keyname), rawFile));
-
+                        final String uuid = UUID.randomUUID().toString();
+//                        final String rawFilename = String.format("/tmp/tmp_%s.wav", uuid);
+//                        final File rawFile = new File(rawFilename);
+//                        final FileOutputStream fileOutputStream = new FileOutputStream(rawFile);
+//                        ByteStreams.copy(stream, fileOutputStream);
+//                        fileOutputStream.close();
+//
+//                        final String s3RawBucket = String.format("%s/%s/%s", s3KeyRaw, synthesizeMessage.getIntent().toString(), synthesizeMessage.getCategory().toString());
+//                        amazonS3.putObject(new PutObjectRequest(s3RawBucket, String.format("%s-raw.wav", keyname), rawFile));
+                        final InputStream newStream = WaveUtils.reWriteWaveHeader(stream);
 
                         // Process Audio
+                        final Optional<AudioInputStream> downSampledAudio = downSampleAudio(newStream, TARGET_SAMPLING_RATE);
+                        if (downSampledAudio.isPresent()) {
+                            // Save to File
+//                            final String processedFilename = String.format("/tmp/tmp_down_%s.wav", uuid);
+//                            final File processedFile = new File(processedFilename);
+//                            final int bytesWritten = AudioSystem.write(downSampledAudio.get(), AudioFileFormat.Type.WAVE, processedFile);
+//                            LOGGER.debug("action=write-downsampled-audio-to-file filename={} size={}", processedFilename, bytesWritten);
+
+                            // get stream size
+                            // final int audioSize = downSampledAudio.get().available();
+                            // LOGGER.debug("audio_size={}", audioSize);
+
+                            // convert input stream to bytes
+                            final byte [] audioBytes = WaveUtils.toByteArray(downSampledAudio.get());
+
+
+                            // write to S3
+                            final String S3Keyname = String.format("%s-16k-java2.wav", keyname);
+                            final String S3Bucket = String.format("%s/%s/%s", s3Key, synthesizeMessage.getIntent().toString(), synthesizeMessage.getCategory().toString());
+                            LOGGER.debug("action=upload-downsampled-s3 bucket={} key={}", S3Bucket, S3Keyname);
+
+                            final ObjectMetadata metadata = new ObjectMetadata();
+                            metadata.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+                            metadata.setContentLength(audioBytes.length);
+
+                            final InputStream s3Stream = new ByteArrayInputStream(audioBytes);
+
+                            final PutObjectResult putResult = amazonS3.putObject(new PutObjectRequest(S3Bucket, S3Keyname, s3Stream, metadata));
+                            LOGGER.debug("action=upload-downsampled-to-S3 md5={}", putResult.getContentMd5());
+
+//                            if (processedFile.delete()) {
+//                                LOGGER.info("action=delete-file-success filename={}", processedFilename);
+//                            } else {
+//                                LOGGER.info("action=delete-file-failure filename={}", processedFilename);
+//                            }
+
+                        }
 
                         // Compress: convert to AD-PCM 16K 6-bit audio and save to a different s3 bucket
                         // "sox test.wav -r 16k -e ima-adpcm -b 4 -c 1 output3.wav";
 //                        final String processedFilename = "/tmp/tmp_compressed.ima";
 //                        final String processCommand = String.format("sox -r 22050 -v 1.5 %s -r 16k -b 4 -c 1 -e ima-adpcm %s", rawFilename, processedFilename);
 
-                        // Downsample: 16k
+                        /**
+                        // Shell out command to downsample: 16k
                         final String processedFilename = "/tmp/tmp_processed.wav";
                         final String processCommand = String.format("sox -r %d %s -r 16k %s", WATSON_SAMPLING_RATE, rawFilename, processedFilename);
 
@@ -169,6 +214,7 @@ public class Text2SpeechQueueConsumer implements Managed {
                         } catch (IOException | InterruptedException e) {
                             LOGGER.error("action=fail-to-sox key={} error={}", keyname, e.getMessage());
                         }
+                         **/
 
                     }
 
@@ -188,6 +234,34 @@ public class Text2SpeechQueueConsumer implements Managed {
         LOGGER.info("key=text2speech-queue-consumer action=consume-text2speech-queue-done");
     }
 
+    private Optional<AudioInputStream> downSampleAudio(final InputStream inputStream, final float targetSampleRate) {
+
+        AudioInputStream sourceStream;
+        try {
+            // final InputStream bufferedStream = new BufferedInputStream(inputStream);
+            sourceStream = AudioSystem.getAudioInputStream(inputStream);
+        } catch (UnsupportedAudioFileException e) {
+            LOGGER.error("error=unsupported-audio-file msg={}", e.getMessage());
+            return Optional.absent();
+        } catch (IOException e) {
+            LOGGER.error("error=io-exception msg={}", e.getMessage());
+            return Optional.absent();
+        }
+
+        final javax.sound.sampled.AudioFormat sourceFormat = sourceStream.getFormat();
+        final javax.sound.sampled.AudioFormat targetFormat = new javax.sound.sampled.AudioFormat(
+                sourceFormat.getEncoding(),
+                targetSampleRate,
+                sourceFormat.getSampleSizeInBits(),
+                sourceFormat.getChannels(),
+                sourceFormat.getFrameSize(),
+                targetSampleRate,
+                sourceFormat.isBigEndian()
+        );
+
+        final AudioInputStream convertedStream = AudioSystem.getAudioInputStream(targetFormat, sourceStream);
+        return Optional.of(convertedStream);
+    }
 
     private List<Text2SpeechMessage> receiveMessages() {
         final ReceiveMessageRequest receiveMessageRequest = new ReceiveMessageRequest()
