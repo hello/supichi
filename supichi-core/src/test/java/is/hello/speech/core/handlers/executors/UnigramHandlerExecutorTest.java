53df7df74c7a1e126f628e80b07271d4e47973ee
Tim Bart <tim@sayhello.com>
2016-08-09 16:26:24 -0700

    Somewhat useful tests

--- supichi-core/src/test/java/is/hello/speech/core/handlers/executors/UnigramHandlerExecutorTest.java
+++ supichi-core/src/test/java/is/hello/speech/core/handlers/executors/UnigramHandlerExecutorTest.java
@@ -1,24 +1,37 @@
 package is.hello.speech.core.handlers.executors;
 
-import com.hello.suripu.core.processors.SleepSoundsProcessor;
-import com.hello.suripu.coredw8.clients.MessejiClient;
 import is.hello.speech.core.db.SpeechCommandDAO;
+import is.hello.speech.core.handlers.TriviaHandler;
+import is.hello.speech.core.models.HandlerResult;
+import is.hello.speech.core.models.HandlerType;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 
 public class UnigramHandlerExecutorTest {
 
     @Test
-    public void TestHandleEmptyTranscript() {
+    public void TestHandleEmptyHandler() {
+
+        final UnigramHandlerExecutor executor = new UnigramHandlerExecutor();
+        final HandlerResult result = executor.handle("123456789", 99L, "whatever");
+        assertEquals(result.handlerType, HandlerType.NONE);
+    }
+
+    @Test
+    public void TestHandleSingleHandler() {
 
         final SpeechCommandDAO speechCommandDAO = mock(SpeechCommandDAO.class);
-        final MessejiClient client = mock(MessejiClient.class);
-        final SleepSoundsProcessor sleepSoundsProcessor = mock(SleepSoundsProcessor.class);
+        final TriviaHandler handler = new TriviaHandler(speechCommandDAO);
+        final HandlerExecutor executor = new UnigramHandlerExecutor()
+                .register(HandlerType.TRIVIA, handler);
 
-//        HandlerFactory factory = HandlerFactory.create();
-//        UnigramHandlerExecutor executor = new UnigramHandlerExecutor()
+        final HandlerResult correctResult = executor.handle("123456789", 99L, "the president");
+        assertEquals(correctResult.handlerType, HandlerType.TRIVIA);
 
+        final HandlerResult result = executor.handle("123456789", 99L, "whatever");
+        assertEquals(result.handlerType, HandlerType.NONE);
     }
 
 }
