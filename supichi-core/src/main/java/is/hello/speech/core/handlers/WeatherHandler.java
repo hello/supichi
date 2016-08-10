834c9978106083d402585a444a6d62646415a235
Tim Bart <tim@sayhello.com>
2016-08-09 17:47:04 -0700

    Add logging

--- supichi-core/src/main/java/is/hello/speech/core/handlers/WeatherHandler.java
+++ supichi-core/src/main/java/is/hello/speech/core/handlers/WeatherHandler.java
@@ -11,11 +11,15 @@ import is.hello.speech.core.db.SpeechCommandDAO;
 import is.hello.speech.core.models.HandlerResult;
 import is.hello.speech.core.models.HandlerType;
 import is.hello.speech.core.models.SpeechCommand;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class WeatherHandler extends BaseHandler {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(WeatherHandler.class);
 
     private static Map<String, SpeechCommand> commands;
     private final AccountLocationDAO accountLocationDAO;
@@ -42,21 +46,30 @@ public class WeatherHandler extends BaseHandler {
     public HandlerResult executeCommand(final String text, final String senseId, final Long accountId) {
         final Optional<AccountLocation> accountLocationOptional = accountLocationDAO.getLastLocationByAccountId(accountId);
         final Map<String,String> params = Maps.newHashMap();
-        params.put("text", "Weather information is not available at this time");
+        final String defaultText = "Weather information is not available at this time";
+        params.put("text", defaultText);
         // 438 shotwell latitude: 37.761185, longitude: -122.416369
 
+        if(accountLocationOptional.isPresent()) {
+            LOGGER.info("action=get-location account_id={} result=found", accountId);
+        }
         final Double latitude = accountLocationOptional.isPresent() ? accountLocationOptional.get().latitude : 37.761185;
         final Double longitude = accountLocationOptional.isPresent() ? accountLocationOptional.get().longitude : -122.416369;
+
+        LOGGER.info("action=get-forecast account_id={} latitude={} longitude={}", accountId, latitude, longitude);
 
         // WARNING: THIS IS A TERRIBLE LIBRARY AND IS NOT THREADSAFE
         final boolean success = forecastIO.getForecast(String.valueOf(latitude), String.valueOf(longitude));
         if(success) {
 
             final FIODaily daily = new FIODaily(forecastIO);
-            params.put("text", String.format("The weather is: %s", daily.getDay(0).summary()));
+            final String responseText = String.format("It is %s", daily.getDay(0).summary());
+            params.put("text", responseText);
+            LOGGER.info("action=get-forecast account_id={} result={}", accountId, responseText);
             return new HandlerResult(HandlerType.WEATHER, params);
         }
 
+        LOGGER.info("action=get-forecast result={}", accountId, defaultText);
         return new HandlerResult(HandlerType.WEATHER, params);
     }
 
