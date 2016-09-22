9182ee3b912cf57abdbf2cdededcaff20973eaaf
kingshy <kingshy@sayhello.com>
2016-09-22 12:40:18 -0700

    White Noise

--- supichi-core/src/main/java/is/hello/speech/core/handlers/SleepSoundHandler.java
+++ supichi-core/src/main/java/is/hello/speech/core/handlers/SleepSoundHandler.java
@@ -98,6 +98,7 @@ public class SleepSoundHandler extends BaseHandler {
         tempMap.put("play sleep sound", SpeechCommand.SLEEP_SOUND_PLAY);
         tempMap.put("play sleep", SpeechCommand.SLEEP_SOUND_PLAY);
         tempMap.put("white noise", SpeechCommand.SLEEP_SOUND_PLAY);
+        tempMap.put("White Noise", SpeechCommand.SLEEP_SOUND_PLAY);
 
         tempMap.put("stop", SpeechCommand.SLEEP_SOUND_STOP);
         tempMap.put("stop sound", SpeechCommand.SLEEP_SOUND_STOP);
